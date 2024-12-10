4942a5e438fd0793fb0dbf95f287d322d3743f9c
geopaul94 <geopaul94@gmail.com>
2024-12-10 16:29:57 +0530

    excel sheet added .

--- lib/presentation/screeens/sales_screen/excel_file.dart
+++ lib/presentation/screeens/sales_screen/excel_file.dart
@@ -1,101 +1,179 @@
 // import 'dart:io';
+
+// import 'package:flutter/cupertino.dart';
 // import 'package:flutter/material.dart';
-// import 'package:excel/excel.dart';
+// import 'package:google_fonts/google_fonts.dart';
+
+// import 'package:syncfusion_flutter_xlsio/xlsio.dart' as xcel;
 // import 'package:path_provider/path_provider.dart';
-// import 'package:share_plus/share_plus.dart';
 
-// import 'package:inventory_management_system/data/models/sales_model.dart';
-// import 'package:inventory_management_system/presentation/bloc/sales_bloc/sales_bloc.dart';
-// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:permission_handler/permission_handler.dart';
+// import 'package:fluttertoast/fluttertoast.dart';
+// // For File I/O operations
 
-// class ExportToExcelPage extends StatefulWidget {
-//   const ExportToExcelPage({Key? key}) : super(key: key);
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
 
 //   @override
-//   State<ExportToExcelPage> createState() => _ExportToExcelPageState();
+//   State<HomePage> createState() => _HomePageState();
 // }
 
-// class _ExportToExcelPageState extends State<ExportToExcelPage> {
-//   bool _isLoading = false;
-// Future<File> generateExcel(List<SalesDetailsModel> salesData) async {
-//   try {
-//     // Create a new Excel workbook
-//     final Excel excel = Excel.createExcel();
-    
-//     // Remove the default sheet and create a new one
-//     excel.delete('Sheet1');
-//     final Sheet sheet = excel.createSheet('SalesData');
+// class _HomePageState extends State<HomePage> {
+//   List results = Results.results;
 
-//     // Define headers
-//     _safeSetCellValue(sheet, 'A1', 'Sale ID');
-//     _safeSetCellValue(sheet, 'B1', 'Date');
-//     _safeSetCellValue(sheet, 'C1', 'Customer Name');
-//     _safeSetCellValue(sheet, 'D1', 'Amount');
-//     _safeSetCellValue(sheet, 'E1', 'Payment Mode');
-//     _safeSetCellValue(sheet, 'F1', 'Product Name');
-//     _safeSetCellValue(sheet, 'G1', 'Quantity');
-//     _safeSetCellValue(sheet, 'H1', 'Time');
-//     _safeSetCellValue(sheet, 'I1', 'Category');
-//     _safeSetCellValue(sheet, 'J1', 'Created At');
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         elevation: 0.0,
+//         backgroundColor: Colors.transparent,
+//         toolbarHeight: 60,
+//         title: Text(
+//           'All Students',
+//           style: GoogleFonts.montserrat(
+//             textStyle: Theme.of(context).textTheme.headlineMedium,
+//             fontWeight: FontWeight.w400,
+//             fontSize: 22.0,
+//             color: Colors.white,
+//           ),
+//         ),
+//         centerTitle: true,
+//         flexibleSpace: Container(
+//           decoration: const BoxDecoration(
+//             borderRadius: BorderRadius.only(
+//                 bottomLeft: Radius.circular(20),
+//                 bottomRight: Radius.circular(20)),
+//             color: Colors.blue,
+//           ),
+//         ),
+//       ),
+//       body: Container(
+//         color: Colors.white,
+//         child: ListView.separated(
+//           itemCount: results.length,
+//           itemBuilder: (BuildContext context, index) {
+//             return InkWell(
+//               onTap: () {
+//                 Navigator.of(context, rootNavigator: true).push(
+//                   MaterialPageRoute(
+//                     builder: (_) => StudentProfile(
+//                       studentName: results[index][0],
+//                       studentReg: results[index][1],
+//                       studentImage: results[index][2],
+//                       studentResults: results[index][3],
+//                     ),
+//                   ),
+//                 );
 
-//     // Populate sheet with sales data
-//     for (int i = 0; i < salesData.length; i++) {
-//       var sale = salesData[i];
-      
-//       _safeSetCellValue(sheet, 'A${i + 2}', sale.saleId ?? '');
-//       _safeSetCellValue(sheet, 'B${i + 2}', sale.date ?? '');
-//       _safeSetCellValue(sheet, 'C${i + 2}', sale.customerName ?? '');
-//       _safeSetCellValue(sheet, 'D${i + 2}', sale.cash ?? 0.0);
-//       _safeSetCellValue(sheet, 'E${i + 2}', sale.paymentMethod ?? '');
-//       _safeSetCellValue(sheet, 'F${i + 2}', sale.product ?? '');
-//       _safeSetCellValue(sheet, 'G${i + 2}', sale.quantity ?? 0);
-//       _safeSetCellValue(sheet, 'H${i + 2}', sale.Time ?? '');
-//       _safeSetCellValue(sheet, 'I${i + 2}', sale.category ?? '');
-//       _safeSetCellValue(sheet, 'J${i + 2}', 
-//         sale.createdAt != null ? sale.createdAt.toDate().toString() : '');
-//     }
+//                 print("   studentName: ${results[index][0]},");
 
-//     // Save the file
-//     final directory = await getApplicationDocumentsDirectory();
-//     final path = '${directory.path}/SalesData.xlsx';
-    
-//     // Encode and write the file
-//     final List<int>? fileBytes = excel.save();
-//     if (fileBytes != null) {
-//       final File file = File(path)
-//         ..createSync(recursive: true)
-//         ..writeAsBytesSync(fileBytes);
-//       return file;
-//     } else {
-//       throw Exception('Failed to generate Excel file: No file bytes');
-//     }
-//   } catch (e) {
-//     print('Excel generation error: $e');
-//     throw Exception('Failed to generate Excel file: $e');
+//                 print("   studentReg: ${results[index][1]},");
+
+//                 print("   studentImage: ${results[index][2]},");
+
+//                 print("   studentResults: ${results[index][3]},");
+//               },
+//               child: ListTile(
+//                 leading: CircleAvatar(
+//                   backgroundImage: AssetImage(results[index][2]),
+//                 ),
+//                 title: Text(
+//                   results[index][0],
+//                   style: const TextStyle(fontSize: 14),
+//                 ),
+//                 subtitle: Text(
+//                   results[index][1],
+//                   style: const TextStyle(fontSize: 13),
+//                 ),
+//               ),
+//             );
+//           },
+//           separatorBuilder: (BuildContext context, int index) {
+//             return const Divider();
+//           },
+//         ),
+//       ),
+//       floatingActionButton: _excelFAB(),
+//     );
 //   }
-// }
 
-// // Safe cell value setting method
-// void _safeSetCellValue(Sheet sheet, String cellReference, dynamic value) {
-//   try {
-//     final cell = sheet.cell(CellIndex.indexByString(cellReference));
-    
-//     // Handle different types of values
-//     if (value == null) {
-//       cell.value = '';
-//     } else if (value is String) {
-//       cell.value = value;
-//     } else if (value is num) {
-//       cell.value = value;
-//     } else if (value is bool) {
-//       cell.value = value;
-//     } else {
-//       // Convert to string as a last resort
-//       cell.value = value.toString() as CellValue?;
-//     }
-//   } catch (e) {
-//     print('Error setting cell value for $cellReference: $e');
-//     // Optionally log the error or handle it as needed
+//   Widget _excelFAB() {
+//     return FloatingActionButton(
+//       onPressed: () {
+//         generateAndSaveExcel();
+//         print('generate excel sheet pressed');
+//       },
+//       backgroundColor: Colors.green,
+//       child: const Icon(
+//         CupertinoIcons.tray_arrow_down,
+//         color: Colors.white,
+//       ),
+//     );
 //   }
-// }
+
+//   Future<void> generateAndSaveExcel() async {
+//     print('Requesting storage permissions');
+//     // Request storage permissions
+//     var status = await Permission.storage.request();
+//     var manageStatus = await Permission.manageExternalStorage.request();
+//     bool v = true;
+
+//     if (v) {
+//       // Permissions granted
+//       print('Storage permissions granted');
+
+//       // Create a new Excel workbook and worksheet
+//       final xcel.Workbook workbook = xcel.Workbook();
+//       final xcel.Worksheet sheet = workbook.worksheets[0]; // First sheet
+//       const String excelFile = 'students_report.xlsx'; // Name of the Excel file
+
+//       // Populate the Excel sheet with data
+//       sheet.getRangeByIndex(1, 1).setText('All Students');
+//       sheet.getRangeByIndex(2, 1).setText('Form 4 West'); // Example class
+//       sheet.getRangeByIndex(4, 1).setText('Student Name');
+//       sheet.getRangeByIndex(4, 2).setText('Maths');
+//       sheet.getRangeByIndex(4, 3).setText('English');
+//       // Add more columns and data...
+
+//       // Loop through the results to set data in the Excel sheet cells
+//       for (var i = 0; i < results.length; i++) {
+//         sheet.getRangeByIndex(i + 5, 1).setText(results[i][0]);
+//         sheet
+//             .getRangeByIndex(i + 5, 2)
+//             .setText(results[i][3]['Maths'][0].toString());
+//         sheet
+//             .getRangeByIndex(i + 5, 3)
+//             .setText(results[i][3]['English'][0].toString());
+//         // Add more subjects...
+//       }
+
+//       // Save the Excel workbook as a stream of bytes
+//       final List<int> bytes = workbook.saveAsStream();
+
+//       // Get the application documents directory to save the file
+//  //     final directory = await getApplicationDocumentsDirectory();
+
+// final  file = await  File("/storage/emulated/0/Download/$excelFile");
+
+//       // final file = File(
+//       //     "${directory.path}/$excelFile"); // Save the file in the documents directory
+
+//       print('Saving Excel file to: ${file.path}');
+
+//       // Write the bytes to the file
+//       await file.writeAsBytes(bytes);
+
+//       // Show a success message to the user
+//       Fluttertoast.showToast(
+//         msg: 'Excel file successfully saved to: ${file.path}',
+//         toastLength: Toast.LENGTH_LONG,
+//         gravity: ToastGravity.BOTTOM,
+//         backgroundColor: Colors.blue,
+//         textColor: Colors.white,
+//       );
+
+//       // Dispose of the workbook to release memory
+//       workbook.dispose();
+//     }
+//   }
 // }
